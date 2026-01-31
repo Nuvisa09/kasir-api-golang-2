4b5f2c44008e26f4f7d31571beeb056846139341
Nuvisa09 <nurnisa0922@gmail.com>
2026-02-01 01:49:47 +0700

    perubahan main.go

--- main.go
+++ main.go
@@ -23,9 +23,13 @@ type Config struct {
 
 func enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set(
+			"Access-Control-Allow-Headers",
+			"Content-Type, Authorization, X-Requested-With, Accept",
+		)
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
